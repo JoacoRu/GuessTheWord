29b437ed25c2ad981d2d0e1d39051a6c1d932c9d
balas16k <joacorubek@gmail.com>
2018-11-09 12:15:34 -0300

    arreglos de logica de juego

--- index.php
+++ index.php
@@ -8,15 +8,20 @@
 </head>
 <body>
     <form method="post">
-        <input type="text" name="palabra">
+        <input type="text" name="palabraa">
         <button type="submit">Enviar</button>
     </form>
+    <input type="text" name="palabra" value="abcd" hidden>
     <div class="letrasDesordenadas" style="display: flex; flex-direction: row;">
         <p style="margin-right: 40px;">A</p>
         <p style="margin-right: 40px;">B</p>
         <p style="margin-right: 40px;">C</p>
         <p style="margin-right: 40px;">D</p>
-        <p style="margin-right: 40px;">E</p>
+        <p style="margin-right: 40px;">F</p>
+        <p style="margin-right: 40px;">G</p>
+        <p style="margin-right: 40px;">H</p>
+        <p style="margin-right: 40px;">I</p>
+        <p style="margin-right: 40px;">J</p>
     </div>
 
     <script src="js/escojePalabra.js"></script>
--- js/escojePalabra.js
+++ js/escojePalabra.js
@@ -1,18 +1,26 @@
     window.onload = function(){
         var letrasDesordenadas = document.querySelector('.letrasDesordenadas');
         var letrasChildren = letrasDesordenadas.children;
-        var unArray = [];
+        var input = document.querySelector('input[name="palabra"]');
+        var valorInput = input.value;
+        var inputArray = Array.from(valorInput);
+        var unArray = new Array;
         function events(event){
-            unArray.push(this.textContent);
-            console.log('algo');
-            this.style.background = 'grey';
-            this.removeEventListener('click', events);
+            var posicion = inputArray.indexOf(this.textContent.toLowerCase());
+            console.log(posicion);
+            if(posicion != -1){
+                unArray.splice(posicion, 0, this.textContent);
+                this.style.background = 'grey';
+                this.removeEventListener('click', events);
+                console.log(unArray);
+            }else{
+                this.style.background = 'red';
+            }
+            
         }
         for(var i =0; i < letrasChildren.length; i++){
            var event =  letrasChildren[i].addEventListener('click', events);
-            
         }
 
-
-        console.log(unArray);
+        
     }
